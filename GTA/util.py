75f84f740b707ce76b9fc00310ee5e82b78594ea
cer446 <cer446@nyu.edu>
2016-12-16 13:36:46 -0500

    Deleted redundant functions

--- GTA/util.py
+++ GTA/util.py
@@ -85,12 +85,6 @@ def make_five_year_start(dataset):
     dataset['period'] = [int(i/5)*5 for i in dataset.year]
     return dataset
 
-def create_counts(data, group_by_columns, column_to_count):
-    '''Takes a list of column names and a column to count and counts rows by those column name, including nulls'''
-    all_columns = group_by_columns + [column_to_count]
-    data = data.loc[:,all_columns].fillna(value = 1)
-    grouped = data.groupby(group_by_columns)
-    return grouped.count()
 
 def unstack_table(data):
     '''Unstacks data until there's only one row index remaining'''
@@ -100,9 +94,6 @@ def unstack_table(data):
         data = data.unstack()
         i = i + 1
     return data
-
-def remove_zeros(series):
-    return series[series > 0]
 
 def convert_series(series, label):
     '''Takes a series and label for the series' values, returns df with 2 columns: the series' row index, & the series' values'''
@@ -143,49 +134,3 @@ def replace_series_with_range(data, series_to_group, group_size):
     ranges = create_range(series_to_group, group_size)
     data_replaced = pd.concat([data, pd.DataFrame(ranges)], axis = 1).drop(series_to_group.name, 1)
     return data_replaced
-
-def create_counts(data, group_by_columns, column_to_count):
-    '''Takes a list of column names and a column to count and counts rows by those column name, including nulls'''
-    all_columns = group_by_columns + [column_to_count]
-    data = data.loc[:,all_columns].fillna(value = 1)
-    grouped = data.groupby(group_by_columns)
-    return grouped.count()
-
-def unstack_table(data):
-    '''Unstacks data until there's only one row index remaining'''
-    data = data.fillna(0)
-    i = 0
-    while i < data.index.nlevels:
-        data = data.unstack()
-        i = i + 1
-    return data
-
-def remove_zeros(series):
-    return series[series > 0]
-
-def convert_series(series, label):
-    '''Takes a series and label for the series' values, returns df with 2 columns: the series' row index, & the series' values'''
-    converted = pd.DataFrame(series)
-    converted.columns = [label]
-    converted.reset_index(level=0, inplace=True)
-    return converted
-
-def group_by_columns(data, group_by_columns, column_to_agg):
-    '''Takes a list of column names and a column to count and counts rows by those column name, including nulls'''
-    all_columns = group_by_columns + [column_to_agg]
-    data.loc[:, column_to_agg] = data.loc[:, column_to_agg].fillna(value=0)
-    data = data.loc[:,all_columns]
-    grouped = data.groupby(group_by_columns)
-    return grouped
-
-def sum_by_groups(grouped):
-    '''Takes output of group function and sums data'''
-    sums = grouped.sum().dropna()
-    sums.columns = ['sum']
-    return sums
-
-def count_by_groups(grouped):
-    '''Takes output of group function and counts data'''
-    counts = grouped.count().dropna()
-    counts.columns = ['count']
-    return counts
